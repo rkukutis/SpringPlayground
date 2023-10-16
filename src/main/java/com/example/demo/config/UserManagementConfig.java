9c41a7a90df399579aa43cd690fdf2b0e0deb574
rkukutis <kukutis.ricardas@gmail.com>
2023-10-16 20:33:01 +0300

    Removed duplicate UserDetailsService

--- src/main/java/com/example/demo/config/UserManagementConfig.java
+++ src/main/java/com/example/demo/config/UserManagementConfig.java
@@ -1,32 +1,20 @@
 package com.example.demo.config;
 
-import com.example.demo.services.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.List;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import javax.sql.DataSource;
 
 @Configuration
 public class UserManagementConfig {
 
     @Bean
-    public UserDetailsService userDetailsService(){
-
-        var user = User.withUsername("rkukutis")
-                .password("kukutis")
-                .authorities("huh")
-                .build();
-
-        return new InMemoryUserDetailsService(List.of(user));
-
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
--- src/main/resources/users.sql
+++ src/main/resources/users.sql
@@ -1,15 +1,16 @@
-CREATE TABLE IF NOT EXISTS 'users' (
+CREATE TABLE IF NOT EXISTS users (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 username VARCHAR(45) NOT NULL,
 password VARCHAR(45) NOT NULL,
 enabled INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS 'authority' (
+CREATE TABLE IF NOT EXISTS authorities (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 username VARCHAR(45) NOT NULL,
 authority VARCHAR(45) NOT NULL
 );
 
-INSERT INTO authority(username, authority) VALUES('Rick', 'write');
-INSERT INTO users(username, password, enabled) VALUES('Rick', 'hoopoe', '1');
+INSERT INTO authority(username, authority) VALUES('rkukutis', 'write');
+INSERT INTO users(username, password, enabled) VALUES('rkukutis', 'hoopoe', '1');
+
